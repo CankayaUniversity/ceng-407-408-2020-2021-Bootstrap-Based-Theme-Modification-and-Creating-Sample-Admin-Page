65472d61aaa18256ab3d859fda8851dafba9b59e
Arda <arda_acar_1996@hotmail.com>
2021-12-18 21:36:51 +0000

    Load Chart

---
+++ app/Http/Livewire/Server/Stat/Load.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire\Server\Stat;
+
+use App\Models\SystemResource;
+use Livewire\Component;
+
+class Load extends Component
+{
+    public function render()
+    {
+        return view('livewire.server.stat.load');
+    }
+}
---
+++ resources/views/livewire/server/stat/load.blade.php
@@ -0,0 +1,25 @@
+<div wire:key="load-chart">
+    <!-- Line Chart Starts -->
+    <div class="col-12">
+      <div class="card">
+        <div
+          class="
+            card-header
+            d-flex
+            flex-sm-row flex-column
+            justify-content-md-between
+            align-items-start
+            justify-content-start
+          "
+        >
+          <div>
+            <h4 class="card-title mb-25">Load</h4>
+          </div>
+        </div>
+        <div class="card-body">
+          <div id="load-chart"></div>
+        </div>
+      </div>
+    </div>
+    <!-- Line Chart Ends -->
+</div>
