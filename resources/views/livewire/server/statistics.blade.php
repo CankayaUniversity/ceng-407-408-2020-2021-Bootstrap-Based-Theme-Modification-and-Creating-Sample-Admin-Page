3a5447edc87f4ee0072f163c2db8995d93b8dc2b
Mustafa Aydemir <mustafa@aydemir.im>
2021-12-19 20:45:54 +0000

    upd. stats

--- resources/views/livewire/server/statistics.blade.php
+++ resources/views/livewire/server/statistics.blade.php
@@ -2,9 +2,6 @@
     <div class="card card-statistics">
         <div class="card-header">
             <h4 class="card-title">System Resources</h4>
-            <div class="d-flex align-items-center">
-                <p class="card-text me-25 mb-0">Updated 1 month ago</p>
-            </div>
         </div>
         <div class="card-body statistics-body">
             <div class="row">
@@ -25,12 +22,25 @@
                     <div class="d-flex flex-row">
                         <div class="avatar bg-light-info me-2">
                             <div class="avatar-content">
-                                <i data-feather="user" class="avatar-icon"></i>
+                            <i data-feather="trending-up" class="avatar-icon"></i>
                             </div>
                         </div>
                         <div class="my-auto">
                             <h4 class="fw-bolder mb-0">{{ $system_resource->vmem }}%</h4>
-                            <p class="card-text font-small-3 mb-0">Memory</p>
+                            <p class="card-text font-small-3 mb-0">V. Memory</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-12">
+                    <div class="d-flex flex-row">
+                        <div class="avatar bg-light-success me-2">
+                            <div class="avatar-content">
+                            <i data-feather="trending-up" class="avatar-icon"></i>
+                            </div>
+                        </div>
+                        <div class="my-auto">
+                            <h4 class="fw-bolder mb-0">{{ $system_resource->swap_mem }}%</h4>
+                            <p class="card-text font-small-3 mb-0">S. Memory</p>
                         </div>
                     </div>
                 </div>
@@ -38,7 +48,7 @@
                     <div class="d-flex flex-row">
                         <div class="avatar bg-light-danger me-2">
                             <div class="avatar-content">
-                                <i data-feather="box" class="avatar-icon"></i>
+                            <i data-feather="trending-up" class="avatar-icon"></i>
                             </div>
                         </div>
                         <div class="my-auto">
@@ -47,19 +57,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3 col-sm-6 col-12">
-                    <div class="d-flex flex-row">
-                        <div class="avatar bg-light-success me-2">
-                            <div class="avatar-content">
-                                <i data-feather="dollar-sign" class="avatar-icon"></i>
-                            </div>
-                        </div>
-                        <div class="my-auto">
-                            <h4 class="fw-bolder mb-0">{{ $system_resource->load_avg * 100 }}%</h4>
-                            <p class="card-text font-small-3 mb-0">Load</p>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
         </div>
     </div>
