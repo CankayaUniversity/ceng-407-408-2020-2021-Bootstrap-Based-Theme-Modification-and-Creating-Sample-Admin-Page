fe3fb3ebd9fc3c4f8a1e7cc0f1d8fa43bd1a4f66
Mustafa Aydemir <mustafa@aydemir.im>
2021-12-19 21:35:44 +0000

    installation guide updated

--- resources/views/server/installation-guide.blade.php
+++ resources/views/server/installation-guide.blade.php
@@ -1,5 +1,15 @@
 <div class="card">
     <div class="card-body">
+        <ul class="list-group list-group-numbered mb-2">
+            <li class="list-group-item">Download the <strong>Agent</strong></li>
+            <li class="list-group-item">Open <strong>Task Scheduler</strong> on your <strong>Windows</strong> machine</li>
+            <li class="list-group-item">Follow <strong>Action > Create Basic Task</strong></li>
+            <li class="list-group-item">Enter a <strong>Name</strong>, and click <strong>Next</strong></li>
+            <li class="list-group-item">Select a <strong>period</strong> you want, and click <strong>Next</strong></li>
+            <li class="list-group-item">Select <strong>Start a program</strong>, and click <strong>Next</strong></li>
+            <li class="list-group-item">Select the <strong>Agent</strong>, and click <strong>Next</strong></li>
+            <li class="list-group-item">Click <strong>Finish</strong></li>
+        </ul>
         <a download class="btn btn-primary" href="/api/scheduler/agent/download/{{ $server->key }}">Download Agent</a>
     </div>
 </div>
