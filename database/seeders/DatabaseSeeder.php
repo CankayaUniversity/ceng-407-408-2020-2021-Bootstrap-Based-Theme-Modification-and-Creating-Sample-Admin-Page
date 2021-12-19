1eb9d4f0ad4770f2ebeaa2e6be6f448a1eacaaab
Arda <arda_acar_1996@hotmail.com>
2021-12-19 18:35:38 +0000

    TC* seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -62,17 +62,39 @@ class DatabaseSeeder extends Seeder
             'role' => 'user'
         ]);
 
-        Server::create([
-            'key'         => 'd984c9cd-9938-43f1-843f-89755d89e56c',
-            'user_id'     => 1,
-            'name'        => 'Default Server',
-            'last_update' => now()
-        ]);
 
         Server::create([
             'key'         => '13f3e7fc-5ada-4755-9fc8-781f147b1a1f',
             'user_id'     => 1,
             'name'        => 'Mustafa PC',
+            'last_update' => now()
+        ]);
+
+        Server::create([
+            'key'         => '13f3e7fc-5ada-4755-9fc8-781f147b1a11',
+            'user_id'     => 1,
+            'name'        => 'TC 1',
+            'last_update' => now()
+        ]);
+
+        Server::create([
+            'key'         => '13f3e7fc-5ada-4755-9fc8-781f147b1a12',
+            'user_id'     => 1,
+            'name'        => 'TC 2',
+            'last_update' => now()
+        ]);
+
+        Server::create([
+            'key'         => '13f3e7fc-5ada-4755-9fc8-781f147b1a13',
+            'user_id'     => 1,
+            'name'        => 'TC 3',
+            'last_update' => now()
+        ]);
+
+        Server::create([
+            'key'         => '13f3e7fc-5ada-4755-9fc8-781f147b1a14',
+            'user_id'     => 1,
+            'name'        => 'TC 4',
             'last_update' => now()
         ]);
     }
