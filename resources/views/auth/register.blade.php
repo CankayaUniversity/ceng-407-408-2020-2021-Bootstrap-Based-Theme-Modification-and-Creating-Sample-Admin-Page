76a8921562b48457f4a63decee88884b95d56f25
Arda <arda_acar_1996@hotmail.com>
2022-01-06 13:14:41 +0000

    register ui

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,79 @@
-@extends('layouts.app')
+@extends('layouts/fullLayoutMaster')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<div class="auth-wrapper auth-basic px-2" style="max-width:500px; margin: 0 auto; margin-top: 10vh;">
+  <div class="auth-inner my-2">
+    <!-- Login basic -->
+    <div class="card mb-0">
+      <div class="card-body p-4">
+        <h4 class="card-title mb-1 text-center">Adventure starts here 🚀</h4>
+        <p class="card-text mb-2 text-center">Make your app management easy and fun!</p>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <form class="auth-login-form mt-2" action="{{ route('register') }}" method="POST" novalidate="novalidate" data-bitwarden-watching="1">
+        @csrf  
+        <div class="mb-1">
+            <label for="login-email" class="form-label">Name</label>
+            <input type="text" class="form-control @error('email') is-invalid @enderror" id="login-name" name="name" placeholder="" aria-describedby="login-name" tabindex="1" autofocus="" control-id="ControlID-1" value="{{ old('name') }}">
+            @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror  
         </div>
+        <div class="mb-1">
+            <label for="login-email" class="form-label">Email</label>
+            <input type="text" class="form-control @error('email') is-invalid @enderror" id="login-email" name="email" placeholder="john@example.com" aria-describedby="login-email" tabindex="1" autofocus="" control-id="ControlID-1" value="{{ old('email') }}">
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror  
+        </div>
+
+          <div class="mb-1">
+            <div class="d-flex justify-content-between">
+              <label class="form-label" for="login-password">Password</label>
+            </div>
+            <div class="input-group input-group-merge form-password-toggle">
+              <input type="password" class="form-control form-control-merge @error('password') is-invalid @enderror" id="login-password" name="password" tabindex="2" placeholder="············" aria-describedby="login-password" control-id="ControlID-2">
+              <span class="input-group-text cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg></span>
+              @error('password')
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+              @enderror
+            </div>
+          </div>
+
+          <div class="mb-1">
+            <div class="d-flex justify-content-between">
+              <label class="form-label" for="login-password">Confirm Password</label>
+            </div>
+            <div class="input-group input-group-merge form-password-toggle">
+              <input type="password" class="form-control form-control-merge @error('password') is-invalid @enderror" id="login-password" name="password_confirmation" tabindex="2" placeholder="············" aria-describedby="login-password" control-id="ControlID-2">
+              <span class="input-group-text cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg></span>
+              @error('password')
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+              @enderror
+            </div>
+          </div>
+
+          <button class="btn btn-primary w-100 waves-effect waves-float waves-light" tabindex="4" control-id="ControlID-4">Sign up</button>
+        </form>
+
+        <p class="text-center mt-2">
+          <span>Already have an account?</span>
+          <a href="/login">
+            <span>Sign in instead</span>
+          </a>
+        </p>
+
+        
+      </div>
     </div>
+    <!-- /Login basic -->
+  </div>
 </div>
 @endsection
