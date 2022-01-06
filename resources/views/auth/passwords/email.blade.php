48abeaf2a6c1c7e8f87631755281a4614239a37d
Mustafa Aydemir <mustafa@aydemir.im>
2022-01-06 15:36:50 +0000

    pass reset

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,48 @@
-@extends('layouts.app')
+@extends('layouts/fullLayoutMaster')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<div class="auth-wrapper auth-basic px-2" style="max-width:500px; margin: 0 auto; margin-top: 10vh;">
+    <div class="auth-inner my-2">
+        <!-- Login basic -->
+        <div class="card mb-0">
+            <div class="card-body p-4">
+                <h4 class="card-title mb-1 text-center">Forgot Password? 🔒
+                </h4>
+                <p class="card-text mb-2 text-center">Enter your email and we'll send you instructions to reset your password</p>
 
-                <div class="card-body">
+                <form class="auth-login-form mt-2" action="{{ route('password.email') }}" method="POST" novalidate="novalidate" data-bitwarden-watching="1">
+                    @csrf
+
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
+                    
+                    <div class="mb-1">
+                        <label for="login-email" class="form-label">Email</label>
+                        <input type="text" class="form-control @error('email') is-invalid @enderror" id="login-email" name="email" placeholder="john@example.com" aria-describedby="login-email" tabindex="1" autofocus="" control-id="ControlID-1" value="{{ old('email') }}">
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                    <button class="btn btn-primary w-100 waves-effect waves-float waves-light" tabindex="4" control-id="ControlID-4">Sign in</button>
+                </form>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                <p class="text-center mt-2">
+                    <a href="/login">
+                        <span>Back to login</span>
+                    </a>
+                </p>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
             </div>
         </div>
+        <!-- /Login basic -->
     </div>
 </div>
 @endsection
